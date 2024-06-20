39fdb8bd9f801f4ad1c5c85631a5cadb6d3403a4
ihorSamolich <126615259+ihorSamolich@users.noreply.github.com>
2024-06-20 16:43:54 +0300

    Add lazy loading with delay

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,32 @@
-import CategoriesPage from "pages/categories/Categories.tsx";
-import CreateCategoryPage from "pages/categories/CreateCategory.tsx";
-import EditCategoryPage from "pages/categories/EditCategory.tsx";
 import { Route, Routes } from "react-router-dom";
+import { lazyWithDelay } from "utils/lazyWithDelay.ts";
 
-import Dashboard from "./pages/Dashboard.tsx";
-import Settings from "./pages/Settings.tsx";
 import Layout from "./partials/layouts/Layout.tsx";
 
-function App() {
+const Dashboard = lazyWithDelay(() => import("pages/Dashboard.tsx"));
+const Settings = lazyWithDelay(() => import("pages/Settings.tsx"));
+const CategoriesPage = lazyWithDelay(() => import("pages/categories/Categories.tsx"));
+const CreateCategoryPage = lazyWithDelay(() => import("pages/categories/CreateCategory.tsx"));
+const EditCategoryPage = lazyWithDelay(() => import("pages/categories/EditCategory.tsx"));
+
+const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Dashboard />} />
 
-        <Route path="categories/">
+        <Route path="categories">
           <Route path="list" element={<CategoriesPage />} />
           <Route path="create" element={<CreateCategoryPage />} />
           <Route path="edit/:id" element={<EditCategoryPage />} />
         </Route>
 
-        <Route path="settings/">
+        <Route path="settings">
           <Route path="my-account" element={<Settings />} />
         </Route>
       </Route>
     </Routes>
   );
-}
+};
 
 export default App;
