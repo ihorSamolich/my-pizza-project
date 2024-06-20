0a187fe0ce0bb3b8799ae463f5676ec08ccc92d4
ihorSamolich <126615259+ihorSamolich@users.noreply.github.com>
2024-06-20 16:47:48 +0300

    Fix eslint rules

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -10,6 +10,8 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    'react-hooks/exhaustive-deps': 'off',                // не показувати пропущені залежності в хуках
+    "@typescript-eslint/no-explicit-any": "off",         // не показувати помилки при використанні any
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
